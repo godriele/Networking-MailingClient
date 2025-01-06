07c5c0436417662efeb4695e8775cb7a10a4c02e
Godriele Dela Cruz <godriele11@yahoo.com>
2025-01-05 22:49:53 -0800

    Everything Worked lol, eplained the code a little more

--- app/app.py
+++ app/app.py
@@ -5,11 +5,15 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
-server.ehlo()
+# This will create a SMTP client sessiong connecting to Gmail's SMTP server at port 587
+server.ehlo() # 
 server.starttls()  
-server.login('godriele9@gmail.com', 'Godriele10')  
 
-msg = MIMEMultipart()
+server.login('godriele9@gmail.com', 'Godriele10')  # Email / Password -> Use you own to try 
+
+msg = MIMEMultipart() # Initializes a MIMEMultipart object to create a multipart email message
+# Allows adding both text and attachments
+
 msg['From'] = 'Buko Papi'
 msg['To'] = 'goodydelacruz6@gmail.com'
 msg['Subject'] = 'Just a test'
@@ -17,18 +21,19 @@ msg['Subject'] = 'Just a test'
 with open('msg.txt', 'r') as f:
     message = f.read()
 
-msg.attach(MIMEText(message, 'plain'))
+msg.attach(MIMEText(message, 'plain')) # Attaches the plain text content to the email as a MIME text part
 
 filename = 'lebron.webp'
 with open(filename, 'rb') as attachment:
     p = MIMEBase('application', 'octet-stream')
     p.set_payload(attachment.read())
 
-encoders.encode_base64(p)
+encoders.encode_base64(p) # This will make sure safe transport over email 
 p.add_header('Content-Disposition', f'attachment; filename={filename}')
 msg.attach(p)
 
 text = msg.as_string()
-server.sendmail('godriele9@gmail.com', 'goodydelacruz6@gmail.com', text)
+server.sendmail('godriele9@gmail.com', 'goodydelacruz6@gmail.com', text) 
+# Sends the email!!
 print("It worked")
 server.quit()  
