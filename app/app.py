10949d424c48271a648751bf51b1ef064ece3298
Godriele Dela Cruz <godriele11@yahoo.com>
2025-01-05 22:35:56 -0800

    Added a header, and a txt to my email object

--- app/app.py
+++ app/app.py
@@ -7,4 +7,24 @@ server = smtplib.SMTP('smtp.gmail.com', 25)
 server.ehlo()
 # Call this function to start the process
 
-server.login('goodydelacruz6@gmail.com', 'Godriele9')
+server.login('godriel9@gmial.com', 'Godriele10') # Test account
+
+from email import encoders
+# This module provides utility functions for encoding message payloads for secure transport
+from email.mime.text import MIMEText
+# This calls is used to create MIME (Multipurpose Internet Mail Extensions) objects for plain text email content
+from email.mime.base import MIMEBase
+# This is the base calss for creating MIME-compliant email messages with arbitraty content types
+from email.mime.multipart import MIMEMultipart
+# This class is used to create multipart email messages, which can contain multiple MIME parts
+
+
+msg = MIMEMultipart()
+msg['From'] = 'Buko Papi'
+msg['To'] = 'goodydelacruz6@gmail.com'
+msg['subject'] = 'Just a test'
+
+with open('msg.txt', 'f') as f:
+    message = f.read()
+
+msg.attach(MIMEText(message, 'plain'))
